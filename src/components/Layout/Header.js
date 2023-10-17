2305d36e9b1b77bdfca551ba852c4cf67091f715
Ayush Sahu <ayushsahu050502@gmail.com>
2023-10-17 18:53:58 +0530

    04
    Adding Cart

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,17 +1,23 @@
-import classes from './Header.module.css'
+
+
+import { Fragment } from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
 import mealsimg from '../../Assets/meals.jpg'
-import React from 'react'
-import HeaderCartButton from './HeaderCartButton'
-const Header=props=>{
-    return <React.Fragment>
-        <header className={classes.header}>
-            <h1>Meals</h1>
-            <HeaderCartButton></HeaderCartButton>
-        </header>
-        <div className={classes['main-image']}>
-            <img src={mealsimg} alt='Meals'></img>
-        </div>
-    </React.Fragment>
-}
+import classes from './Header.module.css';
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsimg} alt='A table full of delicious food!' />
+      </div>
+    </Fragment>
+  );
+};
 
 export default Header;
